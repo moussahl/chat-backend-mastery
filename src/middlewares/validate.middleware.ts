3efc2b5c0b77324cf3196058501af40fef182dfc
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-12 18:42:26 +0100

    add validation middleware using express-validator and AppError handling

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,17 @@
+import { validationResult } from "express-validator";
+import AppError from '../utils/AppError.js';
+import {Request, Response, NextFunction} from 'express'
+
+
+//  runs the validators and short-circuits with 400 if invalid.
+const validate = (req: Request, res: Response,next: NextFunction) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+   const message = errors.array().map(e=>e.msg).join(',')
+   return next(new AppError(message, 400))
+  }
+
+  next()
+}
+
+export default validate;
