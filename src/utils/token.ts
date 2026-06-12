a9937322a4e329d6b6c63f56efa55305f0eb6bc5
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-12 19:32:29 +0100

    add enum type for role

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,13 +1,20 @@
 import jwt, { Secret, SignOptions, JwtPayload } from "jsonwebtoken";
 
+//import .env variables
+
 const secret: Secret = process.env.JWT_SECRET || "secret";
 
 const options: SignOptions = {
   expiresIn: (process.env.JWT_EXPIRES_IN as any) || "7d",
 };
 
+
+//define role enum type
+
+export type roleEnum = "admin" | "user";
+
 //sign
-export const signToken = (userId: string, role: string) =>
+export const signToken = (userId: string, role: roleEnum) =>
   jwt.sign({ id: userId, role }, secret, options);
 
 // verify
