78cce9808cd1686a0d67ef0ea96ff644a5eac24e
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-12 17:46:48 +0100

    add global error class

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,15 +1,13 @@
-class AppError extends Error{
-
+class AppError extends Error {
   statusCode: number;
-  isOperational : boolean
+  isOperational: boolean;
 
-constructor(message: string, statusCode: number){
-  super(message);
-  this.statusCode  = statusCode ;
-  this.isOperational = true;
-      Error.captureStackTrace(this, this.constructor);
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
-}
-
 
 export default AppError;
