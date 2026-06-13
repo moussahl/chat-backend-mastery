1f23fc3061e9a3b0fa32540f7d618521a919c716
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-13 22:49:14 +0100

    nothing

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,14 +1,22 @@
 import jwt, { Secret, SignOptions, JwtPayload } from "jsonwebtoken";
 
-//import .env variables
 
+
+interface TokenPayload extends JwtPayload {
+  id: string;
+  role: roleEnum;
+}
+
+
+
+
+//import .env variables
 const secret: Secret = process.env.JWT_SECRET || "secret";
 
 const options: SignOptions = {
   expiresIn: (process.env.JWT_EXPIRES_IN as any) || "7d",};
 
 //define role enum type
-
 export type roleEnum = "admin" | "user";
 
 //sign
@@ -16,6 +24,6 @@ export const signToken = (userId: string, role: roleEnum) =>
   jwt.sign({ id: userId, role }, secret, options);
 
 // verify
-export const verifyToken = (token: string): string | JwtPayload => {
-  return jwt.verify(token, secret);
+export const verifyToken = (token: string): TokenPayload => {
+  return jwt.verify(token, secret) as TokenPayload;
 };
