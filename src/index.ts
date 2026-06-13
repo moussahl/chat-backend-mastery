b87c791f1229c7760c87cd5c83684773516a25d2
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-13 18:12:21 +0100

    mount error handlerto catch any error thrown with next(err)

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,13 @@ require("dotenv").config();
 
 import { connectToDB } from "./config/db.config";
 
+import errorHandler from "./middlewares/error.middleware";
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use(errorHandler)
 
 const startServer = async () => {
   try {
