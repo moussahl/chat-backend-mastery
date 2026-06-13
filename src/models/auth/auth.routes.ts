395b1eaaa427dc7d62418101440357bd6dbcdbf5
Moussa Haloui <hliimoussa2020@gmail.com>
2026-06-13 15:59:57 +0100

    add auth routes

---
+++ src/models/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+import * as authController from './auth.controller'
+import { registerValidator, loginValidator } from './auth.validator.js';
+import validate from '../../middlewares/validate.middleware'
+
+const router = Router()
+
+
+router.post('/register',registerValidator,validate,authController.register )
+router.post('/login',loginValidator, validate, authController.login)
+
+export default router;
