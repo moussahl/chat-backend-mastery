931ba27d321d4ac80e05e09b1635e3e3dcc34e51
Moussa Haloui <hliimoussa2020@gmail.com>
2026-07-02 23:58:46 +0100

    remove authMiddleware from route to add globally

--- src/models/users/user.routes.ts
+++ src/models/users/user.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { authMiddleware } from "./user.controller";
 import * as userController from "./user.controller";
 
 const router = Router();
 
-router.get("/me", authMiddleware, userController.getMe);
-router.patch("/profile", authMiddleware, userController.updateProfile);
-router.patch("/status", authMiddleware, userController.updateStatus);
-router.get("/:userId", authMiddleware,userController.getUserPublicInfo);
-router.get("/users",authMiddleware, userController.getAllusers)
+router.get("/me", userController.getMe);
+router.patch("/profile", userController.updateProfile);
+router.patch("/status", userController.updateStatus);
+router.get("/:userId", userController.getUserPublicInfo);
+router.get("/", userController.getAllusers);
 
 export default router;
