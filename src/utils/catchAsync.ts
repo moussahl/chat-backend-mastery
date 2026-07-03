9d1262e9ba8602aa9c561dae64b83622f3899162
Moussa Haloui <hliimoussa2020@gmail.com>
2026-07-03 05:50:23 +0100

    refactor(utils): make catchAsync generic to retain Express request/response types

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,8 +1,20 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 
 const catchAsync =
-  (fn: RequestHandler): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) => {
+  <
+    P = any,
+    ResBody = any,
+    ReqBody = any,
+    ReqQuery = any,
+    Locals extends Record<string, any> = Record<string, any>,
+  >(
+    fn: RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>,
+  ): RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> =>
+  (
+    req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
+    res: Response<ResBody, Locals>,
+    next: NextFunction,
+  ) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
 
