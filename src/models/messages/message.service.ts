2ce38617999e0947198a3f117b3af10b86d7fde1
Moussa Haloui <hliimoussa2020@gmail.com>
2026-07-08 18:10:29 +0100

    chore(message): add service method placeholders

---
+++ src/models/messages/message.service.ts
@@ -0,0 +1,48 @@
+
+
+
+
+// Save new message
+
+
+
+
+
+
+
+
+// Retrieve message history with pagination
+
+
+
+
+
+// Get message with ID
+
+
+
+
+// Delete message ( only by sender or admin room)
+
+
+
+
+// Mark messages as read
+
+
+
+
+// Get unread messages from a room
+
+
+
+// Delete all messages from a room (when the room is deleted)
+
+
+
+// Search for messages in a room.
+
+
+
+
+// export all services
