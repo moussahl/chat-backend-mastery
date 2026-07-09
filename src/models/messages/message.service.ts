b217c8df0af7943a793ae884ffb19df2fa6fabd3
Moussa Haloui <hliimoussa2020@gmail.com>
2026-07-09 05:54:53 +0100

    feat(message): add get history messages with pagination

--- src/models/messages/message.service.ts
+++ src/models/messages/message.service.ts
@@ -2,7 +2,7 @@ import { RoomMember } from "../rooms/roomMember.model";
 import { isRoomMember } from "../rooms/room.service";
 import AppError from "../../utils/AppError";
 import { Room } from "../rooms/room.model";
-import { Message } from "./message.models";
+import { IMessage, Message } from "./message.models";
 import { Types } from "mongoose";
 
 // Save new message
@@ -54,7 +54,80 @@ export const saveMessage = async (
 
 // Retrieve message history with pagination
 
-// Get message with ID
+export const getHistory = async (
+  userId: string,
+  roomId: string,
+  page = 1,
+  limit = 50,
+) => {
+  // Parameter validation
+  if (page < 1 || limit < 1) {
+    throw new AppError("Page and limit must be positive numbers", 400);
+  }
+
+  if (limit > 100) {
+    throw new AppError("Limit cannot exceed 100", 400);
+  }
+
+  // Verify if the room exists
+  const room = await Room.findById(roomId);
+  if (!room) {
+    throw new AppError("Room not found", 404);
+  }
+
+  // Verify if the user is room member
+  const isMember = isRoomMember(userId, roomId);
+  if (!isMember) {
+    throw new AppError("You are not a member of this room", 403);
+  }
+
+  // Calculate the skip
+  const skip = (page - 1) * limit;
+
+  // Get messages
+  const messages = await Message.find({
+    room: new Types.ObjectId(roomId),
+  })
+    .populate("sender", "username avatar status")
+    .sort({ createdAt: -1 }) // Most messages  recent first
+    .skip(skip)
+    .limit(limit)
+    .lean();
+
+
+
+    
+    // Inverse for get in chronological order
+    messages.reverse();
+ 
+    // Count totla messages number
+    const total = await Message.countDocuments({
+      room: new Types.ObjectId(roomId),
+    });
+ 
+    console.log(
+      `✅ Messages history fetched for room ${roomId}: ${messages.length} messages`
+    );
+ 
+    return {
+      data: messages,
+      pagination: {
+        page,
+        limit,
+        total,
+        pages: Math.ceil(total / limit),
+        hasMore: page < Math.ceil(total / limit),
+      },
+      room: {
+        _id: room._id,
+        name: room.name,
+        type: room.type,
+      },
+    };
+};
+// Get message by ID
+
+
 
 // Delete message ( only by sender or admin room)
 
