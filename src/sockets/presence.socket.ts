f20c91186b5a62b1b887021ff604316023531084
Moussa Haloui <hliimoussa2020@gmail.com>
2026-07-12 05:56:00 +0100

    feat(socket): scaffold presence socket handler with TODO layout

---
+++ src/sockets/presence.socket.ts
@@ -0,0 +1,7 @@
+import { Server, Socket } from "socket.io";
+
+const initPresenceSocket = (io: Server, socket: Socket): void => {
+  // TODO: Implement user presence logic (e.g., tracking online status, joining global/status rooms)
+};
+
+export default initPresenceSocket;
